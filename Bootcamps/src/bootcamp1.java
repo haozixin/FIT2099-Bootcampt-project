02ce5f062f159af25430c7edf14e17102541db9c
Zixin <zhao0004@student.monash.edu>
2021-07-27 11:01:55 +0800

    Initial commit

--- Bootcamps/src/bootcamp1.java
+++ Bootcamps/src/bootcamp1.java
@@ -1,3 +1,9 @@
 public class bootcamp1 {
+    public static void main(String[] args){
+        System.out.println("first tutorial");
+        System.out.println("This line is added by zixin");
+
+
+    }
 
 }
