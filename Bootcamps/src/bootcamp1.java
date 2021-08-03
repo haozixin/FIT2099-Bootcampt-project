35295d6971d812ca173f85d6d7392a37dd147922
Zixin <zhao0004@student.monash.edu>
2021-08-03 14:45:07 +0800

    remove test file

--- Bootcamps/src/bootcamp1.java
+++ Bootcamps/src/bootcamp1.java
@@ -1,8 +1,17 @@
-public class bootcamp1 {
-    public static void main(String[] args){
-        System.out.println("first tutorial");
-        System.out.println("This line is added by zixin");
+import javax.swing.plaf.metal.MetalSliderUI;
 
+public class bootcamp1 {
+    public static void main(String[] args) {
+
+        System.out.println("first tutorial");
+        System.out.println("This line is" +
+                " added" +
+                " by zixin");
+
+        System.out.println("This line is added " +
+                "by zixin");
+        System.out.println("This {{['line']}} is added " + "by zixin");
+        System.out.println("This line is added by zixin");
 
     }
 
